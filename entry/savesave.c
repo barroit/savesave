3760c05dabe6c107d5087b22bd38ef2c0d11186d
Jiamu Sun <barroit@linux.com>
2024-07-13 15:18:05 +0900

    build: change source tree structure
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ entry/savesave.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * Copyright 2024 Jiamu Sun
+ *
+ * Contact: barroit@linux.com
+ */
+
+int main(int argv, const char *argv[])
+{
+	return 0;
+}
---
+++ entry/savesave.cpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * Copyright 2024 Jiamu Sun
+ *
+ * Contact: barroit@linux.com
+ */
+
+#include "windows.h"
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR, int)
+{
+	return 0;
+}
