30656421f7cb63d0bdb05c4a9518570c55d69aef
Jiamu Sun <barroit@linux.com>
2024-08-07 03:11:13 +0900

    fix: add resource file handling for windows build
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- winbuild.ps1
+++ winbuild.ps1
@@ -1,10 +1,19 @@
 # SPDX-License-Identifier: GPL-3.0
+
+if (! $env:MT_PATH) {
+	$message = "`$MT_PATH is undefined, refer to the INSTALL.win for " `
+		    + "configuration instructions"
+	Write-Error $message -Category ObjectNotFound
+	exit 1
+}
+
 if (! (Test-Path ./Kconfig)) {
 	Write-Error "Can't find Kconfig, make sure you are in root directory" `
 		    -Category ObjectNotFound
+	exit 1
 }
 
-$abs_root  = (Get-Item ./Kconfig).Directory.ToString().Replace('\','/')
+$abs_root  = "$pwd".Replace('\','/')
 $abs_root  = $abs_root.Substring(0, $abs_root.Length - 1)
 $abs_build = "$abs_root/build"
 $abs_gen   = "$abs_root/include/generated"
@@ -22,3 +31,6 @@ genconfig --header-path $env:AUTOCONF --config-out .config
 
 cmake -G Ninja $abs_root -B $abs_build
 cmake --build $abs_build --parallel
+
+& "$env:MT_PATH" -nologo -manifest $abs_root/savesave.manifest `
+		 -outputresource:$abs_build/savesave.exe;#1
