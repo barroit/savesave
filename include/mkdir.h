67be62d1e3a59abbb803ba7253eebb3b63cc7d36
Jiamu Sun <barroit@linux.com>
2024-09-15 08:32:22 +0900

    feature: add rmdirr() for recursive file removal
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- include/mkdir.h
+++ include/mkdir.h
@@ -14,10 +14,22 @@ extern "C" {
 
 int mkdirp2(char *name, size_t start);
 
+/**
+ * mkdirp - make directory as well as parent
+ *
+ * return: 0 on success, otherwise an error occurred, and errno is set
+ */
 static inline int mkdirp(char *name)
 {
 	return mkdirp2(name, 0);
 }
+
+/**
+ * rmdirr - remove directory as well as subdirectory
+ *
+ * return: 0 on success, otherwise an error occurred
+ */
+int rmdirr(const char *name);
 
 #ifdef __cplusplus
 }
--- platfree/mkdir.c
+++ platfree/mkdir.c
@@ -8,6 +8,8 @@
 #include "mkdir.h"
 #include "path.h"
 #include "debug.h"
+#include "fileiter.h"
+#include "termas.h"
 
 int mkdirp2(char *name, size_t start)
 {
@@ -37,4 +39,32 @@ int mkdirp2(char *name, size_t start)
 	}
 
 	return 0;
+}
+
+static int do_rmdirr(struct fileiter_file *file, void *data)
+{
+	int err;
+	const char *name = file->absname;
+
+	if (file->is_dir)
+		err = rmdir(name);
+	else
+		err = unlink(name);
+
+	if (err)
+		return warn_errno("failed to remove file `%s'", name);
+
+	return 0;
+}
+
+int rmdirr(const char *name)
+{
+	int ret;
+	struct fileiter iter;
+
+	fileiter_init(&iter, name, do_rmdirr, NULL, FI_LIST_DIR);
+	ret = fileiter_exec(&iter);
+	fileiter_destroy(&iter);
+
+	return ret;
 }
