c4116a5c411560d7dbc56ea238ced2dab3b4cd0d
Jiamu Sun <barroit@linux.com>
2024-10-09 14:08:05 +0900

    chore: simplify no errno version syscall calling convention
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- arch/generic/syscall.h
+++ arch/generic/syscall.h
@@ -10,6 +10,11 @@
 #ifndef ARCH_SYSCALL_H
 #define ARCH_SYSCALL_H
 
+#ifdef __syscall
+# undef __syscall
+# define __syscall syscall_noerrno
+#endif
+
 #define syscall_noerrno(...)			\
 ({						\
 	int errnum = errno;			\
@@ -20,13 +25,5 @@
 	}					\
 	ret;					\
 })
-
-#define __syscall0 syscall_noerrno
-#define __syscall1 syscall_noerrno
-#define __syscall2 syscall_noerrno
-#define __syscall3 syscall_noerrno
-#define __syscall4 syscall_noerrno
-#define __syscall5 syscall_noerrno
-#define __syscall6 syscall_noerrno
 
 #endif /* ARCH_SYSCALL_H */
