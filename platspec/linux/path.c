88e4a7d47fcc8f62fc38bcac82cb04cfb0fc6ad1
Jiamu Sun <barroit@linux.com>
2024-10-11 01:03:59 +0900

    linux: path: replace readlink(2) with readlink_notrunc()
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- platspec/linux/path.c
+++ platspec/linux/path.c
@@ -5,22 +5,18 @@
  * Contact: barroit@linux.com
  */
 
-#include "alloc.h"
+#include "path.h"
 
 const char *get_executable_dirname(void)
 {
 	static const char *path;
 
 	if (!path) {
-		char buf[PATH_MAX];
+		char *target;
+		int err = readlink_notrunc("/proc/self/exe", &target);
 
-		ssize_t nr = readlink("/proc/self/exe",
-				      buf, sizeof_array(buf));
-		BUG_ON(nr == -1 || nr == sizeof_array(buf));
-
-		buf[nr] = 0;
-		path = dirname(buf);
-		path = xstrdup(path);
+		BUG_ON(err);
+		path = target;
 	}
 
 	return path;
