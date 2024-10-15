4512955c6522f212e9ffee128deb2a6b055832b9
Jiamu Sun <barroit@linux.com>
2024-10-15 19:17:16 +0900

    scripts/make-tag: automate tagging process
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ scripts/make-tag.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+
+if [[ ! -f .savesave.example ]]; then
+	>&2 echo 'error: not in source tree'
+	exit 1
+fi
+
+if [[ ! $1 ]]; then
+	>&2 echo 'error: missing version-spec'
+	exit 1
+fi
+
+echo $1 > ./version
+
+git add ./version
+
+git commit -m "Savesave $1"
+
+git tag -sm "Savesave $1" "v$1"
